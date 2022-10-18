dcd0faf7f42dcddd6c72f1dbe55d2dfd14b1a6a5
Angelika <losos878@ukr.net>
2022-10-18 23:50:18 +0300

    hw 26/09

---
+++ hw-26.09/script.js
@@ -0,0 +1,27 @@
+//level1
+const integ = 12;
+const string = '12';
+    console.log(integ == string);
+    console.log(integ === string);
+
+    //level2
+    const sum = 100;
+    const tomat = 10;
+    const cucumbers = 5;
+    const apple = 15;
+    let purchases = (tomat + cucumbers + apple) * 2;
+    let resNuts = sum - purchases;
+    console.log(resNuts);
+    if (purchases < resNuts) {
+        console.log('Больше потратили на орехи');
+        
+    } else {
+        console.log('Больше потратили на фрукты и овощи');
+    }
+
+
+
+        
+    
+
+    
